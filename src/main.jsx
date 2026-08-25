2dc128d17df7c94455f309889c91c81599cf320b
Piyush Ramteke <piyu.143247@gmail.com>
2026-08-25 21:18:45 +0530

    feat: mount Miss Universe collection in app

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import ErrorBoundary from './ErrorBoundary.jsx';
 import FeatureHub from './FeatureHub.jsx';
+import MissUniverseCollection from './MissUniverseCollection.jsx';
 
 function repairPersistedState() {
   try {
@@ -27,6 +28,7 @@ ReactDOM.createRoot(root).render(
     <ErrorBoundary>
       <App />
       <FeatureHub />
+      <MissUniverseCollection />
     </ErrorBoundary>
   </React.StrictMode>
 );
